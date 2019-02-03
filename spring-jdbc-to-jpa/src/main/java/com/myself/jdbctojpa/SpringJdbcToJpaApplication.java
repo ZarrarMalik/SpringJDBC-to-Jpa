99dee12297eb524a71377037adfbee0ca6efc5e2
zarrar <zarrarmalik81@gmail.com>
2019-02-03 16:54:36 +0000

    Jdbc customized RowMapper added with insert/delete etc

--- spring-jdbc-to-jpa/src/main/java/com/myself/jdbctojpa/SpringJdbcToJpaApplication.java
+++ spring-jdbc-to-jpa/src/main/java/com/myself/jdbctojpa/SpringJdbcToJpaApplication.java
@@ -1,5 +1,9 @@
 package com.myself.jdbctojpa;
 
+
+
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.myself.jdbctojpa.entity.Person;
 import com.myself.jdbctojpa.jdbc.PersonJdbcDAO;
 
 @SpringBootApplication
@@ -27,6 +32,14 @@ public class SpringJdbcToJpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("All users -> {}", personJdbcDAO.findAll());
+		logger.info("User id 10001 -> {}",personJdbcDAO.findById(10001));
+		logger.info("Deleting 10002 -> No of Rows Deleted - {}", personJdbcDAO.deleteById(10002));
+		logger.info("Inserting 10004 -> {}", 
+				personJdbcDAO.insert(new Person(10004, "Tara", "Berlin", new Date())));
+		
+		logger.info("Update 10003 -> {}", 
+				personJdbcDAO.update(new Person(10003, "Pieter", "Utrecht", new Date())));
+	
 	}
 
 }
